c761b6ce332b2a99801c0bd539cf1d76b4333c39
= <nessibet@gmail.com>
2022-12-27 17:07:38 +0300

    27.12.22

--- src/main/java/btk/MiniProje.java
+++ src/main/java/btk/MiniProje.java
@@ -1,2 +1,29 @@
-package btk;public class MiniProje {
+package btk;
+
+public class MiniProje {
+    public static void main(String[] args) {
+
+        int sayi=25;
+        int remainder= sayi%2;
+        System.out.println(remainder);
+
+        boolean isprime =true;
+        if (sayi<=1)
+            System.out.println("sayi asal de[ildir");
+
+        for (int i = 2; i < sayi; i++) {
+            if (sayi %i==0){
+                isprime=false ;
+            }
+
+        }
+        if (isprime){
+            System.out.println("sayi asaldir");
+        }else {
+            System.out.println("sayi asal degildir");
+        }
+
+
+
+    }
 }
